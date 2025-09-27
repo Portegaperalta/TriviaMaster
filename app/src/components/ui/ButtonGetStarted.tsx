a8c09a49bb791102697051ad5da46a94b677aa65
Pablo <ortegaperaltap@gmail.com>
2025-09-27 16:16:27 -0400

    add radial bg to get started button

--- app/src/components/ui/ButtonGetStarted.tsx
+++ app/src/components/ui/ButtonGetStarted.tsx
@@ -4,8 +4,8 @@ export default function ButtonGetStarted() {
       <button
         id="getStartedButton"
         type="button"
-        className="text-(--clr-white) text-[1.13rem] font-[600] py-2 px-4 w-full
-        bg-radial-[at_30%_75%] from-(--clr-purple-gradient) to-(--clr-blue-gradient)
+        className="text-(--clr-white) text-[1.13rem] font-[600] py-2 px-4 
+        w-full bg-[radial-gradient(at_0%_0%,_var(--clr-purple-gradient),_var(--clr-blue-gradient)_120%)] 
         rounded-full"
       >
         Let’s Get Started
