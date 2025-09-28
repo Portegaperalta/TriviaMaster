a24766ac4782ceeac3aaf510a42275c12d74c5b9
Pablo <ortegaperaltap@gmail.com>
2025-09-27 21:14:40 -0400

    refactor QuestionCard to accept props to display question data

--- app/src/components/QuestionCard.tsx
+++ app/src/components/QuestionCard.tsx
@@ -1,7 +1,34 @@
-export default function QuestionCard() {
+type QuestionCardProps = {
+  questionNumber: number,
+  questionTitle: string,
+  questionCorrectAnswer: string,
+  questionIncorrectAnswers: string[]
+}
+
+export default function QuestionCard({
+  questionNumber,
+  questionTitle,
+  questionCorrectAnswer,
+  questionIncorrectAnswers
+}: QuestionCardProps) {
   return (
     <div className="question-card">
-
+      <div className="question-number">
+        {`QUESTION ${questionNumber} OF 10`}
+      </div>
+      <div className="question-title">
+        {questionTitle}
+      </div>
+      <div className="question-answers">
+        {
+          questionIncorrectAnswers.map((incorrectAnswer) => (
+            <p>
+              {incorrectAnswer}
+            </p>
+          ))
+        }
+        {questionCorrectAnswer}
+      </div>
     </div>
   )
 }
