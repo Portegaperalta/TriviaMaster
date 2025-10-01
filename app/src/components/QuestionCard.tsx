ce77b2748cdbb3c66acc5ca8b65d3bc8631fa879
Pablo <ortegaperaltap@gmail.com>
2025-09-30 22:40:47 -0400

    add active-index attribute to correct answer element

--- app/src/components/QuestionCard.tsx
+++ app/src/components/QuestionCard.tsx
@@ -50,9 +50,11 @@ export default function QuestionCard({
           ))
         }
         <p
+          data-index={5}
+          onClick={handleAnswerClick}
           dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(questionCorrectAnswer) }}
-          className="text-(--clr-white) text-[1.2rem] border-2 py-2 px-4 rounded-xl
-        cursor-pointer hover:bg-(--clr-light-blue) duration-75 ease-in-out">
+          className={`text-(--clr-white) text-[1.2rem] border-2 py-2 px-4 rounded-xl
+        cursor-pointer ${activeIndex === 5 ? `bg-(--clr-light-blue)` : `bg-transparent`} hover:bg-(--clr-light-blue) duration-75 ease-in-out`}>
         </p>
       </div>
       <ButtonNextQuestion className={`text-(--clr-white) text-[1.13rem] font-[600]
